701d0987780a023d8984e5bd8acfd2ab79a2823d
Dere, Ali Dincer (TR - Istanbul) <adere@deloitte.com>
2020-11-11 00:03:57 +0300

    nio examples

--- io/src/ConsoleSample.java
+++ io/src/ConsoleSample.java
@@ -4,11 +4,14 @@ public class ConsoleSample {
     public static void main(String[] args){
 
         Console console = System.console();
+        System.out.println("hey");
 
         if (console != null) {
             String userInput = console.readLine();
 
             console.writer().println("You entered the following: " + userInput);
+        } else {
+            System.out.println("console null");
         }
 
 
--- nio2/src/PathFirstExample.java
+++ nio2/src/PathFirstExample.java
@@ -1,5 +1,6 @@
 import java.io.Console;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -17,9 +18,28 @@ public class PathFirstExample {
 
 
         System.out.println("The path name is: " + path);
+        System.out.println("File name is :" + path.getFileName());
 
         for(int i=0; i<path.getNameCount(); i++)
             System.out.println("element " + i + "is " + path.getName(i));
+
+        Path parent = path.getParent();
+        while(parent != null) {
+            System.out.println("parent " + parent);
+            parent = parent.getParent();
+        }
+
+        System.out.println("Root is " + path.getRoot());
+
+        System.out.println("is absolute: " + path.isAbsolute());
+
+        System.out.println("subpath 0 to 1 " + path.subpath(0,1));
+
+        try {
+            System.out.println(Paths.get(".").toRealPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
