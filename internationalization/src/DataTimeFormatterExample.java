3896d8aa22017c472644c37325ee998d1bc66429
Ali Dinçer Dere <33752230+alidincerdere@users.noreply.github.com>
2020-12-06 00:01:21 +0300

    Update DataTimeFormatterExample.java

--- internationalization/src/DataTimeFormatterExample.java
+++ internationalization/src/DataTimeFormatterExample.java
@@ -3,11 +3,14 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 public class DataTimeFormatterExample {
     
     public static void main(String[] args) {
 
+
+        //as parameter to format method
         LocalDate localDate = LocalDate.of(2020, Month.DECEMBER, 5);
         LocalTime localTime = LocalTime.of(23,45,10);
         LocalDateTime localDateTime = LocalDateTime.of(localDate, localTime);
@@ -16,5 +19,12 @@ public class DataTimeFormatterExample {
 
         System.out.println(localTime.format(DateTimeFormatter.ISO_LOCAL_TIME));
         System.out.println(localDateTime.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+
+        //format method of formatter itself:
+
+        DateTimeFormatter shortDateTime = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT);
+
+        System.out.println(shortDateTime.format(localDateTime));
     }
 }
